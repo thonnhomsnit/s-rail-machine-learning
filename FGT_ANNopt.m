5e529bd2389b7cabd4dc3121d69952413d409a14
Thonn Homsnit <thonnhomsnit@gmail.com>
2022-10-26 13:56:15 +0700

    update

--- FGT_ANNopt.m
+++ FGT_ANNopt.m
@@ -1,6 +1,6 @@
 close all;
 
-        MultiObj.fun = @ReLu ;
+        MultiObj.fun = @tannet ;
         MultiObj.nVar = 4;
         MultiObj.var_min = [0 0 0 0];
         MultiObj.var_max = [1 1 1 1];
@@ -20,9 +20,12 @@ paretofront = Rfit(1:row,:);
 optimalsolution = R(1:row,:);
 scatter(paretofront(:,1),paretofront(:,2),'b*','LineWidth',3);
 
-MCDMparam.SEA = 0.5;
-MCDMparam.IPF = 0.5;
+paretofront(:,1) = paretofront(:,1).*(max(data(:,5))-min(data(:,5)))+min(data(:,5));
+paretofront(:,2) = paretofront(:,2).*(max(data(:,6))-min(data(:,6)))+min(data(:,6));
 
-MCDM(paretofront,MCDMparam);
-xlim([63 72])
-ylim([-2200 -1600])
+% MCDMparam.SEA = 0.5;
+% MCDMparam.IPF = 0.5;
+% 
+% MCDM(paretofront,MCDMparam);
+% xlim([55 60])
+% ylim([-2350 -2250])
