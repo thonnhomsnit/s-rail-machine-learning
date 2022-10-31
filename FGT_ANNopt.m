c136b02643649fa8fc3318cee34846596d2eff6b
Thonn Homsnit <thonnhomsnit@gmail.com>
2022-10-31 17:19:49 +0700

    what am i doing

--- FGT_ANNopt.m
+++ FGT_ANNopt.m
@@ -1,6 +1,6 @@
 close all;
 
-        MultiObj.fun = @tannet ;
+        MultiObj.fun = @tansig144;
         MultiObj.nVar = 4;
         MultiObj.var_min = [0 0 0 0];
         MultiObj.var_max = [1 1 1 1];
@@ -20,12 +20,12 @@ paretofront = Rfit(1:row,:);
 optimalsolution = R(1:row,:);
 scatter(paretofront(:,1),paretofront(:,2),'b*','LineWidth',3);
 
-paretofront(:,1) = paretofront(:,1).*(max(data(:,5))-min(data(:,5)))+min(data(:,5));
-paretofront(:,2) = paretofront(:,2).*(max(data(:,6))-min(data(:,6)))+min(data(:,6));
+% paretofront(:,1) = paretofront(:,1).*(169.8391-37.1100)+37.1100;
+% paretofront(:,2) = paretofront(:,2).*(2435.1550-729.5885)+729.5885;
 
 % MCDMparam.SEA = 0.5;
 % MCDMparam.IPF = 0.5;
 % 
 % MCDM(paretofront,MCDMparam);
-% xlim([55 60])
-% ylim([-2350 -2250])
+xlim([55 60])
+ylim([-2350 -1800])
