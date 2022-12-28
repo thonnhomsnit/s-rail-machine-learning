64b8b917e2657b10fba815a7ed43c11954fce4ed
Thonn Homsnit <thonnhomsnit@gmail.com>
2022-12-28 14:03:00 +0700

    update

--- ANN_classify.m
+++ ANN_classify.m
@@ -22,9 +22,9 @@ hiddenLayerSize = 10;
 net = patternnet(hiddenLayerSize, trainFcn);
 
 % Setup Division of Data for Training, Validation, Testing
-net.divideParam.trainRatio = 70/100;
-net.divideParam.valRatio = 15/100;
-net.divideParam.testRatio = 15/100;
+net.divideParam.trainRatio = 80/100;
+net.divideParam.valRatio = 20/100;
+net.divideParam.testRatio = 0/100;
 
 % Train the Network
 [net,tr] = train(net,x,t);
